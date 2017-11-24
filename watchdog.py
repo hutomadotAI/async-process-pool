290610ec6b91d780fa98e7cdc7a4d810d7d71afc
Paul Annetts <paul.annetts@hutoma.com>
2017-11-24 12:27:04 +0000

    Merged PR 2005: Add shutdown process that is testable. Fix some race conditions.
    Allow the final kill_process function to be overridden
    with an event raise that a test can look out for.
    Add a separate config timeout value for this for overriding. Only place this is actually done is in the tests however.
    
    Fix race conditions in http.py.
    1) get_ai_statuses_for_api can fail if the training_lookup dictionary is modified during iteration.
    I noticed glitches in the build lab during test runs, which I think is this.
    
    2) Kibana is showing multiple initialization of
    the server in multiple log entries at same millisecond timestamp. Looks like we're not
    following the proper documented on_startup method. Add check to protect against double initialization
    
    Related work items: #3656

--- watchdog.py
+++ watchdog.py
@@ -1,3 +1,4 @@
+"""Watchdog class"""
 import asyncio
 import logging
 
@@ -7,25 +8,33 @@ def _get_logger():
 
 
 class Watchdog:
-    def __init__(self, timeout_seconds, callback, asyncio_loop, name="Unnamed"):
+    """Watchdog class"""
+    def __init__(self, timeout_seconds, callback,
+                 asyncio_loop: asyncio.AbstractEventLoop, name="Unnamed"):
         self.timeout_seconds = float(timeout_seconds)
         self.callback = callback
         self.asyncio_loop = asyncio_loop
         self.logger = _get_logger()
         self.name = name
         self.__call_handle = None
+        self.logger.info(
+            "'%s' watchdog created: will time out after %.2fs",
+            self.name, self.timeout_seconds)
 
     def reset_watchdog(self):
+        """Reset the watchdog"""
         self.cancel()
-        self.__call_handle = self.asyncio_loop.call_later(self.timeout_seconds, self.__internal_callback)
+        self.__call_handle = self.asyncio_loop.call_later(
+            self.timeout_seconds, self.__internal_callback)
 
     def cancel(self):
+        """Cancel the watchdog"""
         if self.__call_handle is not None:
-            self.logger.debug("'{}' watchdog was reset".format(self.name))
+            self.logger.debug("'%s' watchdog was reset", self.name)
             self.__call_handle.cancel()
 
     def __internal_callback(self):
         self.logger.info(
-            "'{}' watchdog timed out after {}s, firing callback".format(self.name,
-                                                                        self.timeout_seconds))
+            "'%s' watchdog timed out after %.2fs, firing callback",
+            self.name, self.timeout_seconds)
         self.callback()
