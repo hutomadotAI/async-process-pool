c9f7f36f28691cc3da4e3aa92f5a5de2d7c0746c
Paul Annetts <paul.annetts@hutoma.com>
2018-09-17 15:59:33 +0000

    Merged PR 3338: Make aiohttp exception wrapper call a utility
    Make aiohttp exception wrapper call a utility
    
    Related work items: #5932

---
+++ aiohttp_wrapped_caller.py
@@ -0,0 +1,32 @@
+import aiohttp
+import traceback
+import logging
+
+
+def _get_logger():
+    logger = logging.getLogger('hu.aiohttp_wrapper_caller')
+    return logger
+
+
+class ExceptionWrappedCaller:
+    """Put an exception logging wrapper around all the endpoints.
+       This is preferable to using aiohttp middleware as we control
+       that here without upstream involvement"""
+
+    def __init__(self, call_to_wrap):
+        self.call_to_wrap = call_to_wrap
+        self.logger = _get_logger()
+
+    async def __call__(self, *args, **kwargs):
+        try:
+            response = await self.call_to_wrap(*args, **kwargs)
+        except aiohttp.web_exceptions.HTTPException:
+            # assume if we're throwing this that it's already logged
+            raise
+        except Exception as exc:
+            self.logger.exception("Unexpected exception in call")
+
+            error_string = "Internal Server Error\n" + traceback.format_exc()
+            raise aiohttp.web_exceptions.HTTPInternalServerError(
+                text=error_string)
+        return response
